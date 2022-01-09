af8ea0e8b0fa6d12a2308b24fa038feedf11bde7
medvedium <mumbles2606@gmail.com>
2022-01-09 13:17:17 +0300

    Filter adaptive

--- src/blocks/modules/catalog-filter/catalog-filter.js
+++ src/blocks/modules/catalog-filter/catalog-filter.js
@@ -16,9 +16,33 @@ if (priceSlider) {
 const filterTitle = document.querySelectorAll('.jsFilterTitle')
 
 if (filterTitle) {
-	filterTitle.forEach(item => {
+	filterTitle.forEach((item) => {
 		item.addEventListener('click', (event) => {
 			item.closest('.catalog-filter__item').classList.toggle('is-open')
+		})
+	})
+}
+
+const filterToggle = document.querySelector('.jsToggleFilter')
+const catalogFilter = document.querySelector('.catalog__filter')
+const catalogFilterClose = document.querySelector('.filter-header__close')
+
+if (filterToggle) {
+	filterToggle.addEventListener('click', () => {
+		catalogFilter.classList.add('is-open')
+		document.body.style.overflow = 'hidden'
+		catalogFilter.addEventListener('click', (event) => {
+			const { target } = event
+			if (target === document.querySelector('.jsCloseFilter')) {
+				catalogFilter.classList.remove('is-open')
+				document.body.style.overflow = 'auto'
+			}
+		})
+		window.addEventListener('keyup', (event) => {
+			if (event.code === 'Escape' && catalogFilter.classList.contains('is-open')) {
+				catalogFilter.classList.remove('is-open')
+				document.body.style.overflow = 'auto'
+			}
 		})
 	})
 }
