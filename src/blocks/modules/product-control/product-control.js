534d6d6f6a07d24f00df4209ba5b006213cf9883
medvedium <advprog2@knit.ru>
2022-01-27 10:40:00 +0300

    Fixes

--- src/blocks/modules/product-control/product-control.js
+++ src/blocks/modules/product-control/product-control.js
@@ -1,5 +1,6 @@
 const productQuantity = document.querySelector('.jsProductQuantity')
 const productMore = document.querySelector('.jsProductMore')
+const productMoreTab = document.querySelector('.jsProductMoreTab')
 
 if (productQuantity) {
 	productQuantity.value += ' шт.'
@@ -8,6 +9,27 @@ if (productQuantity) {
 if (productMore) {
 	productMore.addEventListener('click', (e) => {
 		e.preventDefault()
-		document.querySelectorAll('.product-tabs__title')[0].click()
+		productMoreTab.click()
+	})
+}
+
+const deliveryScrollBtn = document.querySelector('.jsDeliveryScrollBtn')
+const deliveryTab = document.querySelector('.jsDeliveryTab')
+
+if (deliveryScrollBtn) {
+	deliveryScrollBtn.addEventListener('click', () => {
+		deliveryTab.click()
+	})
+}
+
+
+
+const colorChooseBtn = document.querySelector('.jsColorChoose')
+const colorChooseTab = document.querySelector('.jsColorChooseTab')
+
+if (colorChooseBtn) {
+	colorChooseBtn.addEventListener('click', (event) => {
+		event.preventDefault()
+		colorChooseTab.click()
 	})
 }
