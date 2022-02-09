76260fc8c9a46d3f665cf5de48ec17313a01543e
medvedium <mumbles2606@gmail.com>
2022-02-09 15:37:40 +0300

    Thread page some fixes

--- src/blocks/modules/thread/thread.js
+++ src/blocks/modules/thread/thread.js
@@ -2,9 +2,15 @@ const threadBuyBtn = document.querySelector('.jsThreadBuy')
 const threadAddToCart = document.querySelectorAll('.item-thread__to-cart-text')
 
 for (let i = 0; i < threadAddToCart.length; i++) {
-	threadAddToCart[i].addEventListener('click', function() {
+	threadAddToCart[i].addEventListener('click', function () {
 		threadBuyBtn.addEventListener('click', () => {
 			this.closest('.item-thread').classList.add('is-in-cart')
 		})
+
+		const input = document.querySelector('#thread-buy').querySelector('.jsProductQuantity')
+		if (!input.value.includes('боб.')) {
+			input.value = input.value + ' боб.'
+		}
 	})
 }
+
