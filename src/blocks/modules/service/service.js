bd6ca9ffce535ab86ac359f410b060cf5178c9d0
medvedium <advprog2@knit.ru>
2022-03-10 16:47:36 +0300

    Fixes

---
+++ src/blocks/modules/service/service.js
@@ -0,0 +1,22 @@
+const confirmService = document.querySelector('.jsConfirmService')
+
+if (confirmService) {
+  confirmService.addEventListener('click', function(event) {
+    event.preventDefault()
+    this.closest('.service__form').innerHTML = `
+      <div class="form__success-inner">
+            <div class="form__success-icon">
+              <img src="img/big-icons/check.svg" alt="" />
+            </div>
+            <div class="text-center form__success-title mb-2">
+              <div class="h4">Готово</div>
+            </div>
+            <div class="text-center form__success-desc">
+              <div class="text text--color--gray-4">
+                Спасибо за обращение. <br>В ближайшее время с вами свяжется наш менеджер
+              </div>
+            </div>
+          </div>
+    `
+  })
+}
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -24,5 +24,6 @@ import "../../blocks/modules/contacts-page/contacts-page.js";
 import "../../blocks/modules/favorite/favorite-page.js";
 import "../../blocks/modules/favorite-item/favorite-item.js";
 import "../../blocks/modules/favorite-filter/favorite-filter.js";
+import "../../blocks/modules/service/service.js";
 
 import "../../blocks/modules/thread/thread.js";
