ab2b3408c0840ace695963324f9d00e1ce08df1b
medvedium <advprog2@knit.ru>
2022-04-20 11:59:10 +0300

    Product page popup styles fixes

--- src/blocks/modules/partners/partners.js
+++ src/blocks/modules/partners/partners.js
@@ -1,18 +1,34 @@
-const partnersTrigger = document.querySelectorAll(".jsOpenPartnersModal");
+import { Modal } from 'bootstrap';
+
+const partnersTrigger = document.querySelectorAll('.jsOpenPartnersModal')
 
 if (partnersTrigger) {
-  for (let i = 0; i < partnersTrigger.length; i++) {
-    partnersTrigger[i].addEventListener("click", openPartnersModal);
+	for (let i = 0; i < partnersTrigger.length; i++) {
+		partnersTrigger[i].addEventListener('click', openPartnersModal)
+	}
 
-  }
+	function openPartnersModal() {
+		const partnersModal = document.querySelector('#brands')
+		partnersModal.querySelector('.modal-header .h5').textContent = this.dataset.brand
 
-  function openPartnersModal() {
-    const partnersModal = document.querySelector("#brands");
-    partnersModal.querySelector(".modal-header .h5").textContent = this.dataset.brand;
-
-    partnersModal.querySelector('.modal-body').innerHTML = `
+		partnersModal.querySelector('.modal-body').innerHTML = `
       <img src="${this.closest('.partners__item').querySelector('img').src}" alt="">
       ${this.closest('.partners__item').querySelector('.partners__desc').innerHTML}
     `
-  }
+	}
+}
+const pageURL = window.location.href
+if (pageURL.indexOf('#') !== -1) {
+	const hashURL = pageURL.slice(pageURL.indexOf('#') + 1, pageURL.length)
+	const partnersBrands = Array.from(document.querySelectorAll('.jsOpenPartnersModal'))
+	const targetBrand = partnersBrands.filter(item => item.dataset.brand.toLowerCase() === hashURL)
+	const partnersModal = document.querySelector('#brands')
+
+	partnersModal.querySelector('.modal-header .h5').textContent = targetBrand[0].dataset.brand
+	partnersModal.querySelector('.modal-body').innerHTML = `
+      <img src="${targetBrand[0].closest('.partners__item').querySelector('img').src}" alt="">
+      ${targetBrand[0].closest('.partners__item').querySelector('.partners__desc').innerHTML}
+    `
+	let brandModal = new Modal(document.getElementById('brands'), {  })
+	brandModal.show()
 }
